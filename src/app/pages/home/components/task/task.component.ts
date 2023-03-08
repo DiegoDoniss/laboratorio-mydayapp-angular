912795f3d5f81cf4dbbbc1f40fdf07c11cfeb853
diego donis <diego.jdonis@gmail.com>
2023-03-08 08:54:30 -0600

    creacion de componente task

---
+++ src/app/pages/home/components/task/task.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+  @Input() title: string = "";
+  @Input() completed: boolean = false;
+}
